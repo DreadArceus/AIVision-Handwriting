d9673df29082ad8133fe6c291d8d51225e4c68ed
Mayank Talwar <dreadarceus@gmail.com>
2021-01-23 21:57:42 +0530

    feat: data processing

--- ImgProcessing.py
+++ ImgProcessing.py
@@ -1,29 +1,34 @@
 import numpy as np
 import struct
+# from matplotlib import pyplot
 
-file_paths = {
-    'images': './Dataset/train-images-idx3-ubyte',
-    'labels': 'train-labels-idx1-ubyte',
-}
 
-train_img_idx = open(file_paths['images'], 'rb')
-train_img_idx.seek(0)
+def process_images(file_path: str) -> np.ndarray:
+    img_idx = open(file_path, 'rb')
+    img_idx.seek(0)
+    magic_num = struct.unpack('>4B', img_idx.read(4))
+    img_count: int = struct.unpack('>I', img_idx.read(4))[0]
+    row_count: int = struct.unpack('>I', img_idx.read(4))[0]
+    column_count: int = struct.unpack('>I', img_idx.read(4))[0]
+    images: np.ndarray = np.fromfile(
+        img_idx, dtype=np.dtype(np.ubyte).newbyteorder('>'))
+    images = images.reshape(img_count, row_count, column_count)
+    return images
 
-magic_num = struct.unpack('>2BH', train_img_idx.read(4))[2]
-img_count = struct.unpack('>I', train_img_idx.read(4))[0]
-row_count = struct.unpack('>I', train_img_idx.read(4))[0]
-column_count = struct.unpack('>I', train_img_idx.read(4))[0]
 
-for i in range(10):
-    print(f'Image number {i}:')
-    for x in range(row_count):
-        for y in range(column_count):
-            pixel = struct.unpack('>B', train_img_idx.read(1))[0]
-            if pixel < 100:
-                print('0', end='')
-            if pixel < 10:
-                print('0', end='')
-            print(pixel, end=' ')
-        print()
+def process_labels(file_path: str) -> np.ndarray:
+    lb_idx = open(file_path, 'rb')
+    lb_idx.seek(0)
+    magic_num = struct.unpack('>4B', lb_idx.read(4))
+    lb_count: int = struct.unpack('>I', lb_idx.read(4))[0]
+    labels: np.ndarray = np.fromfile(
+        lb_idx, dtype=np.dtype(np.ubyte).newbyteorder('>'))
+    labels = labels.reshape(lb_count)
+    return labels
 
-print(magic_num, img_count, row_count, column_count)
+
+# print(f'Image number {img_count-1}: {images[img_count-1]}')
+# print(magic_num, img_count, row_count, column_count)
+# print(train_labels[7])
+# pyplot.imshow(train_images[7], cmap='gray')
+# pyplot.show()
---
+++ Main.py
@@ -0,0 +1,14 @@
+from ImgProcessing import process_images, process_labels
+
+file_paths = {
+    'images': './Dataset/train-images-idx3-ubyte',
+    'labels': './Dataset/train-labels-idx1-ubyte',
+    'test-images': './Dataset/t10k-images-idx3-ubyte',
+    'test-labels': './Dataset/t10k-labels-idx1-ubyte',
+}
+
+train_images = process_images(file_paths['images'])
+train_labels = process_labels(file_paths['labels'])
+
+test_images = process_images(file_paths['test-images'])
+test_labels = process_labels(file_paths['test-labels'])
