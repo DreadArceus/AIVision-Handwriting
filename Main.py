14d2d5823c022ecd7b324a448a81f4adfd2009c7
Mayank Talwar <dreadarceus@gmail.com>
2021-01-24 00:04:51 +0530

    feat: basic train structure

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from ImgProcessing import process_images, process_labels
+from Model import train
 
 file_paths = {
     'images': './Dataset/train-images-idx3-ubyte',
@@ -9,6 +10,8 @@ file_paths = {
 
 train_images = process_images(file_paths['images'])
 train_labels = process_labels(file_paths['labels'])
+
+train(train_images, train_labels)
 
 test_images = process_images(file_paths['test-images'])
 test_labels = process_labels(file_paths['test-labels'])
--- Model.py
+++ Model.py
@@ -1,1 +1,15 @@
 import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+def train(train_images: np.ndarray, train_labels: np.ndarray) -> None:
+    # pixels = (len(train_images[0]), len(train_images[0][0]))
+    model = keras.Sequential([
+        keras.layers.Reshape(target_shape=(28 * 28), input_shape=(28, 28)),
+        keras.layers.Dense(units=100, activation='relu'),
+        keras.layers.Dense(units=25, activation='relu'),
+        keras.layers.Dense(units=10, activation='softmax'),
+    ])
+    model.compile()
+    model.fit()
